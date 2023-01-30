48456dcbe8bb8f63fa9734a03b9b3a3426185e2f
KALYANJIT DAS <62983087+official-jeetdas@users.noreply.github.com>
2023-01-31 00:26:05 +0530

    update 1.0
    Bugs fixed, details improved etc

--- components/footer.js
+++ components/footer.js
@@ -11,23 +11,28 @@ class footer extends HTMLElement {
                 <div class="f-left col-lg-3 col-md-6 col-sm-12">
                     <img class="footer-logo" src="assets/footer-logo.svg" alt="Logo"> <br>
                     <span class="footer-title">Adv.Pratik Acharjee</span>
+                    <br>
+                    <p class="copyright"> Pratik Acharjee <span><img 
+                    src="assets/copyright.svg" alt="copyright"></span> 2021</p>
+                    
                 </div>
                 <div class="f-right col-lg-9 col-md-6 col-sm-12">
-                    <p class="foot-address">Address: Salt Lake, Kolkata, West Bengal.
-                        <br>
-                        Phone: +91 9876543210.
-                        <br>
-                        Email:pratik@gmail.com.
-                        <br>
-                        <a href="#" target="_blank" rel="noopener noreferrer"><img class="contact-icon"
+                    <p class="foot-address">
+                    Chamber : 32, Kalitara Bose Ln., Beleghata, Kolkata - 700010
+                    .<br><br>
+                    <span class="poweredby"> Powered By: <a  href="https://kalyanjit.me/" target="_blank" rel="noopener noreferrer">Kalyanjit Das</a></span>
+                    <br> 
+                        <a href="https://www.facebook.com/Prateekach" target="_blank" rel="noopener noreferrer"><img class="contact-icon"
                                 src="assets/fb.svg" alt="facebook"></a>
 
-                        <a href="#" target="_blank" rel="noopener noreferrer"><img class="contact-icon"
-                                src="assets/ig.svg" alt="instagram"></a>
+                        <a href="https://www.linkedin.com/in/pratik-acharjee-7b0445224/" target="_blank" rel="noopener noreferrer"><img class="contact-icon"
+                                src="assets/linkedin.svg" alt="Linkedin"></a>
 
                         <a href="#" target="_blank" rel="noopener noreferrer"><img class="contact-icon"
                                 src="assets/tt.svg" alt="tweeter"></a>
+                               
                     </p>
+                   
                 </div>
                 
     </footer>
--- components/header.js
+++ components/header.js
@@ -1,10 +1,10 @@
 class header extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
       <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,12 +24,14 @@ class header extends HTMLElement {
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@1,300&family=Roboto+Mono:wght@200&display=swap"
         rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
     <!-- google fonts link -->
     <!-- Icon CDN -->
     
 
       `;
-    }
   }
-  
-  customElements.define('header-component', header);
+}
+
+customElements.define('header-component', header);
